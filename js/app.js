9caa013bc9d4da5a6ea5b8ed004bb777a0115a02
mr-samsul-alam <contactsamsulalam@gmail.com>
2021-09-02 14:37:15 +0600

    all new

--- js/app.js
+++ js/app.js
@@ -10,36 +10,102 @@ const button = () =>{
         document.getElementById("error-massage").innerHTML =
         "<h5 class='text-center p-3 bg-danger'><b>Please enter a  book Name...</b></h5>";
     }
+
     else{
         const url = `https://openlibrary.org/search.json?q=${inputTextvalue}`
         fetch(url)
         .then(res => res.json())
-        .then(data =>   displaySearchResult(data.docs))
+        .then(data => displaySearchResult(data))
+        
+    
     }
 }
-const displaySearchResult = books =>{
-    // console.log(books);
-    const foundedBook =  document.getElementById('founded-book')
-    foundedBook.textContent ='';
+const displaySearchResult =  books =>{
 
-    books.forEach(book => {
-        console.log(book);
-       const div = document.createElement('div')
-       div.classList.add('col')
-       div.innerHTML =`<div class="card " style="height: 400px; width: 350px" >
-       <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top w-50 mx-auto p-3"   alt="..." >
-       <div class="card-body">
-         <h5 class="card-title">Book Name:${book.title}</h5>
-         <h5 class="card-title"> Author Name:${book.author_name[0]}</h5>
-         <h5 class="card-title">Published Date:${book.publish_date[0]}</h5>
+  const showResultNam = (num) =>{
+    if( num === 0){
+      document.getElementById("error-massage").innerHTML =
+      "<h5 class='text-center p-3 bg-danger'><b>Please enter a  book Nameeew4444...</b></h5>";
+    }
+    else{
+      document.getElementById('search-result-num').innerHTML =`
+      ${num}result Found
+      `
+    }
+   
+  }
+  showResultNam(books.numFound);
+ ///-----books card
 
+
+
+    books.docs.forEach(book => {
+      console.log(book)
+
+
+        const writerName = (name)=>{
+          if(name.author_name === undefined){
+            return 'Author Not Avilable';
+          }
+          else{
+            return `Author Name:${name.author_name[0]}` ;
+          }
+        }
+        
+        const publishName= (nam2) =>{
+          if(nam2.publisherName === undefined){
+            return ' Publisher Not Avilable';
+          }
+          else{
+            return `Publier  Name${nam2.publisherName}`;
+          }
+        }
+  
+        const publishedDate = (date) =>{
+          if(date.first_publish_year === undefined){
+            return 'Publish date Not Avilable'
+          }
+          else{
+            return `First Publish year: ${date.first_publish_year}`
+          }
+        }
+            const foundedBook =  document.getElementById('founded-book')
+            //  foundedBook.textContent ='';
+         const div = document.createElement('div')
+         div.classList.add('col')
+         div.innerHTML =`<div class="card " style="height: 400px; width: 350px" >
+         <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top w-50 mx-auto p-3"   alt="..." >
+         <div class="card-body">
+           <h5 class="card-title">Book Name:${book.title}</h5>
+           <h5 class="card-title">${writerName(book)}</h5>
+           <h5 class="card-title">${publishName(book)}</h5>
+           <h5 class="card-title">${publishedDate(book) }</h5>
+  
+         </div>
        </div>
-     </div>
-       `
-       foundedBook.appendChild(div);
+         `
+         
+         foundedBook.appendChild(div);
+
+    
+      
 
  });
+ console.log('finised')
+
 
   
 }
-// style="width: 500px; height: 500px;"
+
+// //function for displaying error message.
+const errorMessage = () => {
+    console.log('errror kaocos')
+    const errorMessageDiv = document.getElementById("error-massage");
+  
+    errorMessageDiv.innerHTML = ` <div class="card m-auto p-5 bg-warning" style="width: 18rem">
+            <h5 class="card-title">Dear Sir/Ma'am,</h5>
+            <p class="card-text">
+               No Result Found
+            </p>
+          </div>`;
+  };
